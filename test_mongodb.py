5a9ba8c121792508c027c95219a2e65693546090
LAXMAN7795 <laxman.sg0104@gmail.com>
2025-07-04 11:57:59 +0530

    security

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,6 +1,6 @@
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://laxmansg0104:Laxman123@cluster0.tz4nxbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://<username>:<password>@cluster0.tz4nxbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
